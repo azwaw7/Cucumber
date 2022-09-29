be133c9dcf05ab5e54f55af7146fc10940955ed5
yuva <feltanejuba97@gmail.com>
2022-09-28 23:33:17 -0400

    new commit

--- src/test/java/TestRunner/Testrunner.java
+++ src/test/java/TestRunner/Testrunner.java
@@ -4,7 +4,9 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
-
-
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "C:\\Users\\felta\\Desktop\\Cucumber\\src\\Features",
+        glue={"stepDefinitions"})
 public class Testrunner {
 }
--- src/test/java/stepDefinitions/OrangeHRMsteps.java
+++ src/test/java/stepDefinitions/OrangeHRMsteps.java
@@ -17,25 +17,25 @@ public class OrangeHRMsteps {
         driver = new ChromeDriver();
     }
 
-    @Given("I launche chrome Browser")
+    @Given("^I launche chrome Browser$")
     public void i_launche_chrome_browser() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
     }
 
-    @When("I open orange hrm url")
+    @When("^I open orange hrm url$")
     public void i_open_orange_hrm_url() {
         driver.navigate().to("https://opensource-demo.orangehrmlive.com/");
 
     }
 
-    @Then("I verify that logo present on page")
+    @Then("^I verify that logo present on page$")
     public void i_verify_that_logo_present_on_page() {
      Boolean status =   driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[1]/img")).isDisplayed();
         Assert.assertEquals(true,status);
     }
 
-    @Then("close browser")
+    @Then("^close browser$")
     public void close_browser() {
         driver.quit();
 
